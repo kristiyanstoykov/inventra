3f9f2ac6facd8d33dcd8f09a04a4ddcd43758b5a
kristiyanstoykov <krisistoikov@gmail.com>
2025-08-11 16:48:28 +0300

    feat: update New Order page to fetch and display payment types; fix navigation link

--- src/app/admin/orders/new/page.tsx
+++ src/app/admin/orders/new/page.tsx
@@ -6,16 +6,19 @@ import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
 import { Metadata } from 'next';
 import { OrderForm } from '@/components/orders/order-add-form';
+import { getAllPaymentTypes } from '@/db/drizzle/queries/paymentTypes';
 
 export const metadata: Metadata = {
   title: 'New Order',
 };
 
 export default async function NewOrderPage() {
+  const paymentTypesList = await getAllPaymentTypes();
+
   return (
     <div className="flex flex-col items-center mt-4">
       <div className="w-[95%] max-w-full text-left">
-        <Link href="/admin/users/" className="text-blue-600 underline hover:text-blue-800">
+        <Link href="/admin/orders/" className="text-blue-600 underline hover:text-blue-800">
           <ArrowLeft className="inline h-4" /> Go back
         </Link>
       </div>
@@ -24,7 +27,7 @@ export default async function NewOrderPage() {
           <CardTitle>New Order</CardTitle>
         </CardHeader>
         <CardContent className="flex-grow">
-          <OrderForm order={null} brands={[]} attributes={[]} categories={[]} />
+          <OrderForm order={null} paymentTypesList={paymentTypesList} />
         </CardContent>
       </Card>
     </div>
